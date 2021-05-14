9d63c3e0ad031134aa3cb3e68920624fa36a2d94
Paulo Jiménez <pauloajimenezm76@outlook.com>
2021-05-14 03:41:05 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#buttonURL").addEventListener("click", () => {
+    document.querySelector("#url").innerHTML = NewURL();
+  });
 };
+
+function NewURL() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dot = [".me", ".org", ".net", ".us", ".com"];
+
+  let pronounIndx = Math.floor(Math.random() * pronoun.length);
+  let adjIndx = Math.floor(Math.random() * adj.length);
+  let nounIndx = Math.floor(Math.random() * noun.length);
+  let dotIndx = Math.floor(Math.random() * dot.length);
+
+  return pronoun[pronounIndx] + adj[adjIndx] + noun[nounIndx] + dot[dotIndx];
+}
